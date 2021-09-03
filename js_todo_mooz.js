c3033cc9e98c03dccb80fcf84b6cf53afd00fbb9
seramay <lilina172ki@gmail.com>
2021-09-04 06:02:45 +0900

    add_doneEvent_js_todo_mooz
    完了ボタンが生成される毎にイベントを定義するように一部セレクタ名を変更して転記
    それに伴ってモックとして用意していたHTMLを削除、モックてに定義していたイベントも転記の為削除

--- js_todo_mooz.js
+++ js_todo_mooz.js
@@ -1,15 +1,6 @@
-const buttonList = document.querySelectorAll('.done_button');
-// 取得したNodeListオブジェクト「buttonList」をArrayオブジェクトに変換
-// 思わぬ動作を防止する為に配列オブジェクトに変換している
-[...buttonList].forEach((button) => {
-  button.addEventListener('click', () => {
-    const list = button.closest('li');
-    list.classList.add('todo_done');
-  })
-})
-
 const todoList = document.querySelector('#todo_list');
 const addTodoButton = document.querySelector('#add_button');
+
 addTodoButton.addEventListener('click', () => {
   const input = document.querySelector('input');
   const text = input.value;
@@ -21,6 +12,11 @@ addTodoButton.addEventListener('click', () => {
   const doneButton = document.createElement('button');
   doneButton.innerText = 'done'
   doneButton.classList.add('done_button');
+  
+  doneButton.addEventListener('click', () => {
+    const list = doneButton.closest('li');
+    list.classList.add('todo_done');
+  })
 
   todoList.appendChild(li);
   li.appendChild(doneButton);
