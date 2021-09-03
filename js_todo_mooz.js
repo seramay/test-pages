8377cfd741b30a0cc4bedd7ec73631f28140f7dc
seramay <lilina172ki@gmail.com>
2021-09-04 05:10:23 +0900

    update_event_js_todo_mooz
    先頭のリスト取得から全てのリスト要素を取得に変更
    繰り返し構文で各リスト要素にイベントを定義

--- js_todo_mooz.js
+++ js_todo_mooz.js
@@ -1,5 +1,8 @@
-const button = document.querySelector('button');
-const list = document.querySelector('li')
-button.addEventListener('click', () => {
-  list.classList.add('todo_done');
+const buttonList = document.querySelectorAll('button');
+// 取得したNodeListオブジェクト「buttonList」をArrayオブジェクトに変換し、forEachで反復適用
+[...buttonList].forEach((button) => {
+  button.addEventListener('click', () => {
+    const list = button.closest("li");
+    list.classList.add('todo_done');
+})
 })
