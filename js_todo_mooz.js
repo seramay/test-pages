1d6585cfb7d32ff52722c6266cc27afec7a24886
seramay <lilina172ki@gmail.com>
2021-09-04 05:47:30 +0900

    add_input_js_todo_mooz
    インプット欄を実装
    インプット欄に記入した内容を取得
    JSでli要素の作成をし、取得した内容をテキストとして渡し、CSS適用の為のクラス付与
    その後にul要素に生成された内容を追加

--- js_todo_mooz.js
+++ js_todo_mooz.js
@@ -1,8 +1,20 @@
-const buttonList = document.querySelectorAll('button');
-// 取得したNodeListオブジェクト「buttonList」をArrayオブジェクトに変換し、forEachで反復適用
+const buttonList = document.querySelectorAll('.done_button');
+// 取得したNodeListオブジェクト「buttonList」をArrayオブジェクトに変換
+// 思わぬ動作を防止する為に配列オブジェクトに変換している
 [...buttonList].forEach((button) => {
   button.addEventListener('click', () => {
-    const list = button.closest("li");
+    const list = button.closest('li');
     list.classList.add('todo_done');
+  })
 })
+
+const todoList = document.querySelector('#todo_list');
+const addTodoButton = document.querySelector('#add_button');
+addTodoButton.addEventListener('click', () => {
+  const input = document.querySelector('input');
+  const text = input.value;
+  const li = document.createElement('li');
+  li.innerText = text
+  li.classList.add('todo');
+  todoList.appendChild(li);
 })
