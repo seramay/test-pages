5923090fdf1a6e4ed4448ded83ec924283786fc4
seramay <lilina172ki@gmail.com>
2021-09-07 10:23:53 +0900

    init_quiz.js_and_mock
    クイズアプリ用のJSファイル作成
    表示するクイズの内容をJSに記述しHTMLに反映するコードの追加

---
+++ quiz.js
@@ -0,0 +1,16 @@
+const question = '日本で最も多いとされている血液型は次のうちどれ？';
+const answers = [
+  'A型',
+  'B型',
+  'O型',
+  'AB型'
+];
+const correct = 'A型'
+
+document.querySelector('#js-question').textContent = question; 
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
+
+// クリックイベントで答え合わせ
