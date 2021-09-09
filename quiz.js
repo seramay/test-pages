de3658f6ed7bfbed5ab1e79711de8a950d265010
seramay <lilina172ki@gmail.com>
2021-09-09 10:16:03 +0900

    Update_event_quiz.js
    全てのイベントをとりあえず実装

--- quiz.js
+++ quiz.js
@@ -17,8 +17,33 @@ $button[2].textContent = answers[2];
 $button[3].textContent = answers[3];
 
 // クリックイベントで答え合わせ
+// ※ 要リファクタリング
 $button[0].addEventListener('click', () => {
   if(correct === $button[0].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+})
+
+$button[1].addEventListener('click', () => {
+  if(correct === $button[1].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+})
+
+$button[2].addEventListener('click', () => {
+  if(correct === $button[2].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+})
+
+$button[3].addEventListener('click', () => {
+  if(correct === $button[3].textContent){
     window.alert('正解！');
   } else {
     window.alert('不正解！');
