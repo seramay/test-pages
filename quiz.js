3b32d14412c84928d1f2d645b6819107cfb86400
seramay <lilina172ki@gmail.com>
2021-09-09 09:59:32 +0900

    add_event_quiz.js
    イベントの実装

--- quiz.js
+++ quiz.js
@@ -13,4 +13,11 @@ document.getElementsByTagName('button')[1].textContent = answers[1];
 document.getElementsByTagName('button')[2].textContent = answers[2];
 document.getElementsByTagName('button')[3].textContent = answers[3];
 
-// クリックイベントで答え合わせ 15:46
+// クリックイベントで答え合わせ
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+  if(correct === document.getElementsByTagName('button')[0].textContent){
+    window.alert('正解！');
+  } else {
+    window.alert('不正解！');
+  }
+})
